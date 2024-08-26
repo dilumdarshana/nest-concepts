96f1f2d94eb60958cb647b549312c57d5358574d
Dilum Darshana <dilum.dar@gmail.com>
2024-08-26 08:15:01 +0530

    End to end testing

--- carval/test/auth.e2e-spec.ts
+++ carval/test/auth.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('AuthController (e2e)', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
+  it ('handles a signup request', () => {
     const email = 'd.xyz1@test.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
@@ -28,5 +28,23 @@ describe('AuthController (e2e)', () => {
         expect(res.body.email).toEqual(email);
         expect(res.body.id).toBeDefined();
       }))
+  });
+
+  it ('signup as a new user then get current logged in user', async () => {
+    const email = 'user@example.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123' })
+      expect(201)
+
+      const cookie = res.get('Set-Cookie');
+
+      const { body } = await request(app.getHttpServer())
+        .get('/auth/whoami')
+        .set('Cookie', cookie)
+        .expect(200)
+
+      expect(body.email).toEqual(email);
   });
 });
